cebef15787975d9a8c94ff7fb220f14fef9f0e9e
Herman Adu <herman.adu@hotmail.com>
2024-07-16 20:47:18 +0100

    Added shadcn tabs and implemented tabs for content, store and analytics tab screens

--- src/app/secret-dashboard/page.tsx
+++ src/app/secret-dashboard/page.tsx
@@ -1,10 +1,27 @@
 import BaseLayout from '@/components/BaseLayout'
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 
 const Page = () => {
   return (
     <BaseLayout renderRightPanel={false}>
-      Admin Dashboard page
+      
+      <Tabs defaultValue='content' className='w-full mx-auto my-10 px-2 md:px-10'>
+        <TabsList className='flex flex-col md:flex-row w-full md:w-3/4 mx-auto h-auto'>
+        <TabsTrigger value='content' className='w-full md:w-auto'>
+						Content
+					</TabsTrigger>
+					<TabsTrigger value='store' className='w-full md:w-auto'>
+						Store
+					</TabsTrigger>
+					<TabsTrigger value='analytics' className='w-full md:w-auto'>
+						Analytics
+					</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+
     </BaseLayout>
   )
 }
