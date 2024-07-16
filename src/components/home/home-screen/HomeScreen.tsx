8b3aaa4f0fd0cd2969a3973bb737e467ef9d4708
Herman Adu <herman.adu@hotmail.com>
2024-07-16 09:42:54 +0100

    built post, posts and post skeleton components, added to completed home screen component

--- src/components/home/home-screen/HomeScreen.tsx
+++ src/components/home/home-screen/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import BaseLayout from "@/components/BaseLayout"
 import UserProfile from "./UserProfile"
+import Posts from "./Posts";
 
 
 const HomeScreen = () => {
   return (
     <BaseLayout>
       <UserProfile />
+      <Posts />
     </BaseLayout>
   )
 }
