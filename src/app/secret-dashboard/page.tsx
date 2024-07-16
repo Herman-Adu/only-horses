0465b2a2c53c8e38b838e501449c117c6f19cc13
Herman Adu <herman.adu@hotmail.com>
2024-07-16 20:19:27 +0100

    Added empty admin dashboard page, with default title inside base layout removing the right panel with prop

---
+++ src/app/secret-dashboard/page.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from '@/components/BaseLayout'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <BaseLayout renderRightPanel={false}>
+      Admin Dashboard page
+    </BaseLayout>
+  )
+}
+
+export default Page
