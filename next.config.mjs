c429bf6d41d89e202c6c8d6a1ebdc67319525074
Herman Adu <herman.adu@hotmail.com>
2024-07-17 11:27:02 +0100

    Completed content tab, implemented media upload for Cloudinary, public access and create post button.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+	images: {
+		// dont allow others to use your next js server to optimise images
+		remotePatterns: [{ hostname: "res.cloudinary.com" }],
+	},
+};
 
 export default nextConfig;
--- src/components/home/auth-screen/TodaysHighlight.tsx
+++ src/components/home/auth-screen/TodaysHighlight.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CldVideoPlayer } from "next-cloudinary";
-import "next-cloudinary/dist/cld-video-player.css";
+
 
 const TodaysHighlight = () => {
   return (
