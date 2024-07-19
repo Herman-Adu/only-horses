6652576954cc4d8ad265b1f4ace5d2a7ab19b0bf
Herman Adu <herman.adu@hotmail.com>
2024-07-19 17:10:15 +0100

    Build out UI for the update profile form component

---
+++ src/app/update-profile/page.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from "@/components/BaseLayout";
+import UpdateProfileForm from "./UpdateProfileForm";
+
+const Page = () => {
+  return (
+    <BaseLayout>
+      <UpdateProfileForm />
+    </BaseLayout>
+  );
+};
+
+export default Page;
